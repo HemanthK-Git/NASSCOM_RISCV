5e8f4a3fcc74efd54511247c772d65ab5e33c111
TEEDA HEMANTH KUMAR <hemanthkumar199925@gmail.com>
2025-03-28 13:32:22 +0530

    Create day_3.cmd

---
+++ day_3.cmd
@@ -0,0 +1,21 @@
+Day 3: combinational circuit lab
+---------------------------------------
+1. Inverter: https://www.makerchip.com/sandbox/0G6fJhkg2/0nZh7jL
+2. 2input gates: https://makerchip.com/sandbox/0J6f8hv6J/0pghnKW
+3. 2x1_mux: https://makerchip.com/sandbox/0J6f8hv6J/0qjh8lQ
+4. Calculator: https://makerchip.com/sandbox/0J6f8hv6J/0r0h8oM
+
+Day 3: Sequential circuit
+------------------------------
+1. Fibonacci and counter: https://makerchip.com/sandbox/0J6f8hv6J/0vgh7K3
+2. calculator with sequential logic: https://makerchip.com/sandbox/0J6f8hv6J/0wjhG91
+
+Day 3: Pipelining
+-----------------------
+1. Pythagoras theorem: https://www.makerchip.com/sandbox/0G6fJhkg2/0y8hrkx
+2. Pipeline errors lab: https://www.makerchip.com/sandbox/0G6fJhkg2/0zmhM9k
+3. Pipeline lab1 - calc with pipeline and counter: https://www.makerchip.com/sandbox/0G6fJhkg2/0k5hO1J
+4. pipeline lab2 - calc with valid: https://www.makerchip.com/sandbox/0G6fJhkg2/0lOh2n5
+5. pipeline lab3 - calc with valid_or_reset: https://www.makerchip.com/sandbox/0G6fJhkg2/0mwhjJj
+6. pipeline lab4 - calc with mem: https://www.makerchip.com/sandbox/0G6fJhkg2/0nZh7OG
+7. Hierarchy lab - 3_coordinate distance: https://www.makerchip.com/sandbox/0G6fJhkg2/0oYhrqD
