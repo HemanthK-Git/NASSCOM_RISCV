a7a237b7d8da6039b8c0cc2316c4c9033d671bd3
TEEDA HEMANTH KUMAR <hemanthkumar199925@gmail.com>
2025-03-28 17:55:13 +0530

    Create day_5.cmd

---
+++ day_5.cmd
@@ -0,0 +1,11 @@
+DAY 5: PIPELINED RISC_V CPU 
+
+1. single cycle cpu, valid signal: https://www.makerchip.com/sandbox/0lYfoh13w/0k5hOWJ
+2. cpu,validity check, pc/branch modification:  https://www.makerchip.com/sandbox/0lYfoh13w/0lOh275
+3.  3 stage pipeline: https://www.makerchip.com/sandbox/0lYfoh13w/0mwhj2j
+4. 3 stage pipeline  reg bypass:  https://www.makerchip.com/sandbox/0lYfoh13w/0nZh75G
+5.  pipeline reg bypass  branch correction: https://www.makerchip.com/sandbox/0lYfoh13w/0oYhr4D
+6.  pipeline  hazard handling  decode other instrs (except load): https://www.makerchip.com/sandbox/0lYfoh13w/0pghnDQ
+7.  pipeline  hazard handling  complete decode + all alu (except load store): https://www.makerchip.com/sandbox/0lYfoh13w/0qjh8W7
+8.  pipeline  hazard handling complete decode + complete alu (with load store): https://www.makerchip.com/sandbox/0lYfoh13w/0r0h892
+9. Final : https://www.makerchip.com/sandbox/0lYfoh13w/0vgh7XL
