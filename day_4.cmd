76e857eb6594393fa754f24d96f1be3448f0aaae
TEEDA HEMANTH KUMAR <hemanthkumar199925@gmail.com>
2025-03-28 14:04:56 +0530

    Create day_4.cmd

---
+++ day_4.cmd
@@ -0,0 +1,13 @@
+DAY 4: SINGLE CYCLE RISC_V CPU USING TL_VERILOG
+
+1. Program counter: https://www.makerchip.com/sandbox/0G6fJhkg2/0qjh807
+2. pc & instr mem access:  https://www.makerchip.com/sandbox/0G6fJhkg2/0r0h8V2
+3. pc & instr fetch + instr format decode:  https://www.makerchip.com/sandbox/0G6fJhkg2/0vgh7NL
+4. pc & instr fetch + opcode decode + imm retrieval:  https://makerchip.com/sandbox/0J6f8hv6J/0xGh1MB
+5. pc & instr fetch + instr decode - opcode, imm, other fields:  https://www.makerchip.com/sandbox/0G6fJhkg2/0y8hrXA
+6. pc & instr fetch + decode unit (with validity check):  https://www.makerchip.com/sandbox/0G6fJhkg2/0zmhMgE
+7. pc & instr fetch + decode unit (validity check + individual instr decode):  https://www.makerchip.com/sandbox/0G6fJhkg2/0Bgh763
+8. pc & instr fetch + complete decode unit + reg file read:  https://www.makerchip.com/sandbox/0G6fJhkg2/0DRh5vr
+9. pc & instr fetch + complete decode + reg file read/write + alu:  https://www.makerchip.com/sandbox/0G6fJhkg2/0Elh39A
+10. pc & instr fetch + decode + reg r/w + alu + taken_br: https://www.makerchip.com/sandbox/0G6fJhkg2/0GZh1Yj
+11. Single cycle risc_v that computes the sum of numbers from 1 to 9:https://www.makerchip.com/sandbox/0G6fJhkg2/0JZhqr5
